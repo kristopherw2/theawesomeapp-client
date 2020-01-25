c9aa3dbc89ffbe511c0b25bd16358e266dfb1a6d
Carl Ortutay <carlorty@gmail.com>
2020-01-25 17:00:10 -0500

    Total slices earned is complete. Comparison component sidlined for now.

--- src/User-info/UserInfoMain.js
+++ src/User-info/UserInfoMain.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import { Component } from 'react';
-import Nav from '../Components/Nav/Nav';
-import CompareWorkouts from './ComapreWorkouts';
-import PastWorkouts from './PastWorkouts';
-import TotalPizzaEarned from './TotalPizzaEarned'
-import UserStats from './UserStats'
-
+import React from "react";
+import {Component} from "react";
+import Nav from "../Components/Nav/Nav";
+import PastWorkouts from "./PastWorkouts";
+import TotalPizzaEarned from "./TotalPizzaEarned";
+import UserStats from "./UserStats";
 
 export default class UserInfo extends Component {
-    render() {
-        return (
-            <div>
-                <Nav />
-                <CompareWorkouts />
-                <PastWorkouts />
-                <TotalPizzaEarned />
-                <UserStats />
-            </div>
-        )
-        }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <UserStats />
+        <TotalPizzaEarned />
+        <PastWorkouts />
+      </div>
+    );
+  }
 }
