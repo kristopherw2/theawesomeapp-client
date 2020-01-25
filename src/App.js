6b69284bc8e78f6a2dc83fa1d1f15294deed266a
Carl Ortutay <carlorty@gmail.com>
2020-01-24 23:20:42 -0500

    Added Nav and Footer components. Landing Page written

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Component} from "react";
+import Landing from "./Landing/Landing";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Landing />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,10 +1,47 @@
 import React from "react";
 import "./Landing.css";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
 function Landing() {
-  return <div className='Landing'>
-  
-  </div>;
+  return (
+    <div className='Landing'>
+      <Nav />
+      <section>
+        <p>
+          Since the dawn of time pizza and fitness have been at odds. Pizza with
+          its heavy amounts of grease, bread, cheese, and fat. While fitness on
+          the other hand...well honestly if we have to explain it’s benefits
+          then it may be time to go back to grade school gym class. No matter
+          your education level Fitness Pizza aims to join these now former
+          adversaries together to help you slide your way into better health.
+          It’s simple, really! Tell Fitness pizza how hard, or soft, you trained
+          that day. It then bakes all that data into a tangible, understandable,
+          result:
+        </p>
+
+        <p>
+          <b>Slices of delicious pizza</b>.
+        </p>
+
+        <p>
+          Ok, not actually real pizza, but it will tell you, based off your
+          calories burned, how many slices you burned off that body of yours.
+          Giving you the power to know, for sure, if you really did earn that
+          coveted cheat meal.
+        </p>
+
+        <p>
+          Because, c’mon, isn’t that really all you want to know from a fitness
+          tracker?
+        </p>
+
+        <button type='submit'>Earn some slices?</button>
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Landing;
