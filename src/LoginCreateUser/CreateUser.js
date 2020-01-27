c20734538a29ff09701ebc53ef92aee0a552c321
Carl Ortutay <carlorty@gmail.com>
2020-01-26 21:38:55 -0500

    updated create user form to add age & height

--- src/LoginCreateUser/CreateUser.js
+++ src/LoginCreateUser/CreateUser.js
@@ -1,20 +1,23 @@
 import React from "react";
-import "../App.css";
+import "./CreateUser.css";
 import {Component} from "react";
 import {Link} from "react-router-dom";
 
 class CreateUser extends Component {
   render() {
     return (
-      <div>
-        <h3>Create User</h3>
-        <form className='create_user'>
+      <div className='create_user'>
+        <h3 className='title'>Create User</h3>
+        <form className='create_form'>
           <p>Username:</p>
           <input type='text' />
           <p>Password:</p>
           <input type='text' />
-
-          <Link to={"/homepage"}>
+          <p>Age:</p>
+          <input type='text' />
+          <p>Height:</p>
+          <input type='text' />
+          <Link to={"/homepage"} id='btn'>
             <button>Let's go!</button>
           </Link>
         </form>
