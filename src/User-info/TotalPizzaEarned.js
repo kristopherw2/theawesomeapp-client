35da1d4112d4f4c16d647e8145d220f1256fab5a
Carl Ortutay <carlorty@gmail.com>
2020-01-26 23:13:57 -0500

    DIRTY COMMIT: Homepage CSS not complete.

--- src/User-info/TotalPizzaEarned.js
+++ src/User-info/TotalPizzaEarned.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./TotalPizzaEarned.css";
 
 const slices = (
   <span role='img' aria-label='A slice of pizza'>
@@ -9,11 +9,9 @@ const slices = (
 
 function TotalPizzaEarned() {
   return (
-    <div>
-      <>
-        <h3>Lifetime Slices Earned:</h3>
-        <p>#  {slices}'s</p>
-      </>
+    <div className='pizza_dudes_got_30_seconds'>
+      <h3>Lifetime Slices Earned:</h3>
+      <p id='grab_some'># {slices}'s</p>
     </div>
   );
 }
