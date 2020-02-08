f8506fcdd8a1c0ebe5326d520b8e6053eb5d0c61
Carl Ortutay <carlorty@gmail.com>
2020-02-08 04:47:12 -0500

    Converted UserStats.js to class component. Added context.

--- src/User-info/UserStats/UserStats.js
+++ src/User-info/UserStats/UserStats.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./UserStats.css";
+import {Component} from "react";
+import UserContext from "../../UserContext";
 
-function UserStats(props) {
-  return (
-    <div className='userStats'>
-      <ul>
-        <li>Username:</li>
-        <li>Age:</li>
-        <li>Height:</li>
-        <li>Weight:</li>
-      </ul>
-    </div>
-  );
+class UserStats extends Component {
+  static contextType = UserContext;
+
+  render() {
+    console.log(`Loook it worked !!! ${this.context.id}`);
+    console.log(`Loook it worked !!! ${this.context.username}`);
+    return (
+      <div className='userStats'>
+        <ul>
+          <li>Username:</li>
+          <li>Age:</li>
+          <li>Height:</li>
+          <li>Weight:</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default UserStats;
