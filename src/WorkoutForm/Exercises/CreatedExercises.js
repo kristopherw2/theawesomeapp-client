94f919cccf80e31ebdbb9e1c049e6823c5c29684
Carl Ortutay <carlorty@gmail.com>
2020-02-15 10:47:20 -0500

    removed key in list error

--- src/WorkoutForm/Exercises/CreatedExercises.js
+++ src/WorkoutForm/Exercises/CreatedExercises.js
@@ -1,75 +1,71 @@
 import React from "react";
 import {Component} from "react";
 //import {Link} from "react-router-dom";
-import UserContext from '../../UserContext'
+import UserContext from "../../UserContext";
 
 class CreatedExercises extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            exercisesArray: []
-        }
-    }
+    this.state = {
+      exercisesArray: [],
+    };
+  }
 
-    static contextType = UserContext;
+  static contextType = UserContext;
 
-    handleDelete = exerciseid => {
-        let newExercisesArray = [];
-        this.context.exercisesArray.filter(item => {
-        return item.exerciseid !== exerciseid ? newExercisesArray.push(item) : null;
-        });
-    ;
-    
-        const url = `http://localhost:8000/api/exercises/${exerciseid}`;
-        const options = {
-        method: "DELETE",
-        };
-    
-        fetch(url, options)
-        .then(res => {
-            if (!res.ok) {
-            throw new Error("Oh, Mamma Mia! There seems to be a problem.");
-            }
-            return res;
-        })
-        .then(data => {
-            this.context.handleDeleteExercise(newExercisesArray)
-            this.setState({
-                workoutsArray: data
-            })
-        })
-        .catch(err => {
-            this.setState({
-            error: err.message,
-            });
-        });
+  handleDelete = exerciseid => {
+    let newExercisesArray = [];
+    this.context.exercisesArray.filter(item => {
+      return item.exerciseid !== exerciseid
+        ? newExercisesArray.push(item)
+        : null;
+    });
+    const url = `http://localhost:8000/api/exercises/${exerciseid}`;
+    const options = {
+      method: "DELETE",
     };
 
-    render() {
+    fetch(url, options)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Oh, Mamma Mia! There seems to be a problem.");
+        }
+        return res;
+      })
+      .then(data => {
+        this.context.handleDeleteExercise(newExercisesArray);
+        this.setState({
+          workoutsArray: data,
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message,
+        });
+      });
+  };
 
-        const newExerciseDisplay = this.context.exercisesArray
+  render() {
+    const newExerciseDisplay = this.context.exercisesArray;
 
-        const exercisesArray = newExerciseDisplay.map((item, index) => {
-            return (
-                    <ul>
-                        <li key={index}>Name: {item.exercisename}</li>
-                        <li key={index+1}>Sets: {item.sets}</li>
-                        <li key={index+2}>Weight lbs: {item.exerciseweight} total</li>
-                        <li key={index+3}> Time: {item.time} seconds</li>
-                        <li key={index+4}>Calories: {item.caloriesburned}</li>
-                        <button onClick={() => this.handleDelete(item.exerciseid)}>Delete</button>
-                    </ul>
-                    
-            )
-        } ) 
+    const exercisesArray = newExerciseDisplay.map((item, index) => {
+      return (
+        <ul>
+          <li key={index}>Name: {item.exercisename}</li>
+          <li key={index + 1}>Sets: {item.sets}</li>
+          <li key={index + 2}>Weight lbs: {item.exerciseweight} total</li>
+          <li key={index + 3}> Time: {item.time} seconds</li>
+          <li key={index + 4}>Calories: {item.caloriesburned}</li>
+          <button key={index + 5} onClick={() => this.handleDelete(item.exerciseid)}>
+            Delete
+          </button>
+        </ul>
+      );
+    });
 
-        return ( 
-            <div className='results-container'>
-                {exercisesArray} 
-            </div>
-        )
-    }
+    return <div className='results-container'>{exercisesArray}</div>;
+  }
 }
 
-export default CreatedExercises
+export default CreatedExercises;
