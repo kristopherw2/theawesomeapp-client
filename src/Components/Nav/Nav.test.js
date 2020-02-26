4a33131f5c9572621eec2fd9bb747de001b7b861
Carl Ortutay <carlorty@gmail.com>
2020-02-25 19:46:01 -0500

    Tests passing for App.js, Nav.js, Footer,js.

--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import Nav from "./Nav";
+
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
+  ReactDOM.render(<Nav />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
