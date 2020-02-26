68e5b1b1e96163c1b545084e23c9164abe736709
Carl Ortutay <carlorty@gmail.com>
2020-02-25 19:50:55 -0500

    PageNotFound.js test passing

---
+++ src/Components/PageNotFound.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PageNotFound from "./PageNotFound";
+
+
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<PageNotFound />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
